2776dd08cd2e41834928e21a97bee0c44fec57c3
mcbeem <mmcbee@gmail.com>
2019-12-12 19:47:45 -0500

    add loading of GPArotation package back in

--- Code/analysis.R
+++ Code/analysis.R
@@ -40,10 +40,9 @@
 #  a mid-2014 Macbook Pro (Core i5-2600ghz). Plan accordingly!
 #
 # sessionInfo() -----------------------------------------------------------
-# > sessionInfo() for Mac (OSX)
 # R version 3.6.1 (2019-07-05)
 # Platform: x86_64-apple-darwin15.6.0 (64-bit)
-# Running under: macOS Catalina 10.15
+# Running under: macOS Catalina 10.15.2
 # 
 # Matrix products: default
 # BLAS:   /System/Library/Frameworks/Accelerate.framework/Versions/A/Frameworks/vecLib.framework/Versions/A/libBLAS.dylib
@@ -53,25 +52,28 @@
 #   [1] en_US.UTF-8/en_US.UTF-8/en_US.UTF-8/C/en_US.UTF-8/en_US.UTF-8
 # 
 # attached base packages:
-#   [1] grid      stats     graphics  grDevices utils     datasets  methods   base     
+# [1] grid      stats     graphics  grDevices utils     datasets  methods   base     
 # 
 # other attached packages:
-#   [1] gridGraphics_0.4-1  ggExtra_0.9         extrafont_0.17      tibble_2.1.3       
-# [5] dplyr_0.8.3         multcomp_1.4-10     TH.data_1.0-10      MASS_7.3-51.4      
-# [9] mvtnorm_1.0-11      psych_1.8.12        cowplot_1.0.0       stringr_1.4.0      
-# [13] mice_3.6.0          data.table_1.12.6   reshape2_1.4.3      broom_0.5.2        
-# [17] stargazer_5.2.2     PSAgraphics_2.1.1   rpart_4.1-15        ggplot2_3.2.1      
-# [21] twang_1.5           latticeExtra_0.6-28 RColorBrewer_1.1-2  lattice_0.20-38    
-# [25] xtable_1.8-4        survey_3.36         survival_2.44-1.1   Matrix_1.2-17      
-# [29] gbm_2.1.5           tidyr_1.0.0         here_0.1           
+# [1] GPArotation_2014.11-1 gridGraphics_0.4-1    ggExtra_0.9          
+# [4] extrafont_0.17        tibble_2.1.3          dplyr_0.8.3          
+# [7] multcomp_1.4-10       TH.data_1.0-10        MASS_7.3-51.4        
+# [10] mvtnorm_1.0-11        psych_1.8.12          cowplot_1.0.0        
+# [13] stringr_1.4.0         mice_3.6.0            data.table_1.12.6    
+# [16] reshape2_1.4.3        broom_0.5.2           stargazer_5.2.2      
+# [19] PSAgraphics_2.1.1     rpart_4.1-15          ggplot2_3.2.1        
+# [22] twang_1.5             latticeExtra_0.6-28   RColorBrewer_1.1-2   
+# [25] lattice_0.20-38       xtable_1.8-4          survey_3.36          
+# [28] survival_2.44-1.1     Matrix_1.2-17         gbm_2.1.5            
+# [31] tidyr_1.0.0           here_0.1             
 # 
 # loaded via a namespace (and not attached):
-#   [1] splines_3.6.1    shiny_1.4.0      assertthat_0.2.1 Rttf2pt1_1.3.7   pillar_1.4.2    
+# [1] splines_3.6.1    shiny_1.4.0      assertthat_0.2.1 Rttf2pt1_1.3.7   pillar_1.4.2    
 # [6] backports_1.1.5  glue_1.3.1       extrafontdb_1.0  digest_0.6.23    promises_1.1.0  
-# [11] minqa_1.2.4      colorspace_1.4-1 sandwich_2.5-1   httpuv_1.5.2     htmltools_0.4.0 
+# [11] minqa_1.2.4      colorspace_1.4-1 sandwich_2.5-1   htmltools_0.4.0  httpuv_1.5.2    
 # [16] plyr_1.8.4       pkgconfig_2.0.3  purrr_0.3.3      scales_1.1.0     later_1.0.0     
 # [21] lme4_1.1-21      generics_0.0.2   withr_2.1.2      pan_1.6          nnet_7.3-12     
-# [26] lazyeval_0.2.2   mnormt_1.5-5     mime_0.7         magrittr_1.5     crayon_1.3.4    
+# [26] lazyeval_0.2.2   mnormt_1.5-5     magrittr_1.5     crayon_1.3.4     mime_0.7        
 # [31] mitml_0.3-7      nlme_3.1-141     foreign_0.8-72   tools_3.6.1      mitools_2.4     
 # [36] lifecycle_0.1.0  munsell_0.5.0    packrat_0.5.0    compiler_3.6.1   rlang_0.4.2     
 # [41] nloptr_1.2.1     rstudioapi_0.10  miniUI_0.1.1.1   boot_1.3-22      gtable_0.3.0    
@@ -79,7 +81,7 @@
 # [51] fastmap_1.0.1    zeallot_0.1.0    jomo_2.6-9       rprojroot_1.3-2  stringi_1.4.3   
 # [56] parallel_3.6.1   Rcpp_1.0.3       vctrs_0.2.0      tidyselect_0.2.5
 
-# also tested on Windows 10 machine with following sessionInfo()
+# also tested on a Windows 10 machine with following sessionInfo()
 
 # > sessionInfo()
 # R version 3.6.1 (2019-07-05)
@@ -89,25 +91,25 @@
 # Matrix products: default
 # 
 # locale:
-#   [1] LC_COLLATE=English_United States.1252  LC_CTYPE=English_United States.1252   
+# [1] LC_COLLATE=English_United States.1252  LC_CTYPE=English_United States.1252   
 # [3] LC_MONETARY=English_United States.1252 LC_NUMERIC=C                          
 # [5] LC_TIME=English_United States.1252    
 # 
 # attached base packages:
-#   [1] grid      stats     graphics  grDevices utils     datasets  methods   base     
+# [1] grid      stats     graphics  grDevices utils     datasets  methods   base     
 # 
 # other attached packages:
-#   [1] gridGraphics_0.4-1  ggExtra_0.9         extrafont_0.17      tibble_2.1.3       
-# [5] dplyr_0.8.3         multcomp_1.4-10     TH.data_1.0-10      MASS_7.3-51.4      
-# [9] mvtnorm_1.0-11      psych_1.8.12        cowplot_1.0.0       stringr_1.4.0      
-# [13] mice_3.6.0          data.table_1.12.6   reshape2_1.4.3      broom_0.5.2        
-# [17] stargazer_5.2.2     PSAgraphics_2.1.1   rpart_4.1-15        ggplot2_3.2.1      
-# [21] twang_1.5           latticeExtra_0.6-28 RColorBrewer_1.1-2  lattice_0.20-38    
-# [25] xtable_1.8-4        survey_3.36         survival_2.44-1.1   Matrix_1.2-17      
-# [29] gbm_2.1.5           tidyr_1.0.0         here_0.1           
+# [1] GPArotation_2014.11-1 gridGraphics_0.4-1    ggExtra_0.9           extrafont_0.17       
+# [5] tibble_2.1.3          dplyr_0.8.3           multcomp_1.4-10       TH.data_1.0-10       
+# [9] MASS_7.3-51.4         mvtnorm_1.0-11        psych_1.8.12          cowplot_1.0.0        
+# [13] stringr_1.4.0         mice_3.6.0            data.table_1.12.6     reshape2_1.4.3       
+# [17] broom_0.5.2           stargazer_5.2.2       PSAgraphics_2.1.1     rpart_4.1-15         
+# [21] ggplot2_3.2.1         twang_1.5             latticeExtra_0.6-28   RColorBrewer_1.1-2   
+# [25] lattice_0.20-38       xtable_1.8-4          survey_3.36           survival_2.44-1.1    
+# [29] Matrix_1.2-17         gbm_2.1.5             tidyr_1.0.0           here_0.1             
 # 
 # loaded via a namespace (and not attached):
-#   [1] splines_3.6.1    shiny_1.4.0      assertthat_0.2.1 Rttf2pt1_1.3.7   pillar_1.4.2    
+# [1] splines_3.6.1    shiny_1.4.0      assertthat_0.2.1 Rttf2pt1_1.3.7   pillar_1.4.2    
 # [6] backports_1.1.5  glue_1.3.1       extrafontdb_1.0  digest_0.6.22    promises_1.1.0  
 # [11] minqa_1.2.4      colorspace_1.4-1 sandwich_2.5-1   httpuv_1.5.2     htmltools_0.4.0 
 # [16] plyr_1.8.4       pkgconfig_2.0.3  purrr_0.3.3      scales_1.1.0     later_1.0.0     
@@ -153,6 +155,7 @@
 # install.packages("extrafont")
 # install.packages("ggExtra")
 # install.packages("gridGraphics")
+# install.packages("GPArotation")
 
 # load the required packages
 library(here)
@@ -175,6 +178,7 @@ library(tibble)
 library(extrafont)
 library(ggExtra)
 library(gridGraphics)
+library(GPArotation)
 
 # Make your system fonts available to R; only needs to be run once
 #  this can take a few minutes, so I've commented it out
---
+++ Dockerfile
@@ -0,0 +1,42 @@
+FROM rocker/rstudio:latest
+MAINTAINER Matt McBee (mmcbee@gmail.com)
+
+# grant read/write permissions for the project directories
+ADD --chown=rstudio /Code/analysis.R /home/rstudio/Code/analysis.r
+ADD  --chown=rstudio /Code/NLSY_CYA_rawdata_import.R /home/rstudio/Code/NLSY_CYA_rawdata_import.r
+ADD  --chown=rstudio /Code/NLSY_rawdata_import.R /home/rstudio/Code/NLSY_rawdata_import.r
+
+ADD  --chown=rstudio /Data/NLSY_CYA_raw.dat /home/rstudio/Data/NLSY_CYA_raw.dat
+ADD  --chown=rstudio /Data/NLSY_raw.dat /home/rstudio/Data/NLSY_raw.dat
+
+# install build dependencies for data.table
+RUN apt-get update && apt-get install zlib1g-dev
+
+# move readme files to these directories. This is a workaround to give rstudio write permission
+#  to these locations. dir.create followed by Sys.chmod did not accomplish this
+ADD --chown=rstudio /Manuscript/Figures/readme.txt  /home/rstudio/Manuscript/Figures/readme.txt
+ADD --chown=rstudio /Manuscript/Tables/readme.txt  /home/rstudio/Manuscript/Tables/readme.txt
+ADD --chown=rstudio /Results/readme.txt  /home/rstudio/Results/readme.txt
+
+# install the necessary packages
+RUN R -e "install.packages('PSAgraphics', repos = 'http://cran.us.r-project.org')"
+RUN R -e "install.packages('here', repos = 'http://cran.us.r-project.org')"
+RUN R -e "install.packages('tidyr', repos = 'http://cran.us.r-project.org')"
+RUN R -e "install.packages('twang', repos = 'http://cran.us.r-project.org')"
+RUN R -e "install.packages('survey', repos = 'http://cran.us.r-project.org')"
+RUN R -e "install.packages('ggplot2', repos = 'http://cran.us.r-project.org')"
+RUN R -e "install.packages('stargazer', repos = 'http://cran.us.r-project.org')"
+RUN R -e "install.packages('broom', repos = 'http://cran.us.r-project.org')"
+RUN R -e "install.packages('reshape2', repos = 'http://cran.us.r-project.org')"
+RUN R -e "install.packages('data.table', repos = 'http://cran.us.r-project.org')"
+RUN R -e "install.packages('mice', repos = 'http://cran.us.r-project.org')"
+RUN R -e "install.packages('stringr', repos = 'http://cran.us.r-project.org')"
+RUN R -e "install.packages('cowplot', repos = 'http://cran.us.r-project.org')"
+RUN R -e "install.packages('psych', repos = 'http://cran.us.r-project.org')"
+RUN R -e "install.packages('multcomp', repos = 'http://cran.us.r-project.org')"
+RUN R -e "install.packages('dplyr', repos = 'http://cran.us.r-project.org')"
+RUN R -e "install.packages('tibble', repos = 'http://cran.us.r-project.org')"
+RUN R -e "install.packages('extrafont', repos = 'http://cran.us.r-project.org')"
+RUN R -e "install.packages('ggExtra', repos = 'http://cran.us.r-project.org')"
+RUN R -e "install.packages('gridGraphics', repos = 'http://cran.us.r-project.org')"
+RUN R -e "install.packages('GPArotation', repos = 'http://cran.us.r-project.org')"
